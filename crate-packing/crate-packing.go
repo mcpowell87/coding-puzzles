f8393e2f8767c79be51f068646fcf547ab187a2b
Michael Powell <>
2019-05-03 20:24:53 -0400

    Added solution to crate-packing fit2

--- crate-packing/crate-packing.go
+++ crate-packing/crate-packing.go
@@ -8,7 +8,8 @@ import (
 // From https://www.reddit.com/r/dailyprogrammer/comments/bazy5j/20190408_challenge_377_easy_axisaligned_crate/
 
 /*
- DESCRIPTION fit1():
+ DESCRIPTION
+ fit1():
  You have a 2-dimensional rectangular crate of size X by Y, and a bunch of boxes, each of size x by y.
  The dimensions are all positive integers.  Given X, Y, x, and y, determine how many boxes can fit into
  a single crate if they have to be placed so that the x-axis of the boxes is aligned with the x-axis of
@@ -18,6 +19,11 @@ import (
  For instance, if the crate is size X = 25 by Y = 18, and the boxes are size x = 6 by y = 5, then the answer is 12.
  You can fit 4 boxes along the x-axis (because 6*4 <= 25), and 3 boxes along the y-axis (because 5*3 <= 18), so in
  total you can fit 4*3 = 12 boxes in a rectangle.
+
+ fit2():
+ You upgrade your packing robot with the latest in packing technology: turning stuff. You now have the option of
+ rotating all boxes by 90 degrees, so that you can treat a set of 6-by-5 boxes as a set of 5-by-6 boxes. You do
+ not have the option of rotating some of the boxes but not others.
 */
 
 func main() {
@@ -27,10 +33,23 @@ func main() {
 	fmt.Println(fit1(12, 34, 5, 6))
 	fmt.Println(fit1(12345, 678910, 1112, 1314))
 	fmt.Println(fit1(5, 100, 6, 1))
+	fmt.Println("Fit2")
+	fmt.Println(fit2(25, 18, 6, 5))
+	fmt.Println(fit2(12, 34, 5, 6))
+	fmt.Println(fit2(12345, 678910, 1112, 1314))
+	fmt.Println(fit2(5, 5, 3, 2))
+	fmt.Println(fit2(5, 100, 6, 1))
+	fmt.Println(fit2(5, 5, 6, 1))
 }
 
 // Returns the number of boxes with the specified size that will fit into the specified size crate
 // Assuming the x-axis of the boxes are aligned with the x-axis of the crate
 func fit1(crateX int, crateY int, boxesX int, boxesY int) float64 {
 	return math.Floor(float64(crateX/boxesX)) * math.Floor(float64(crateY/boxesY))
+}
+
+// Returns the number of boxes with the specified size that will fit into the specified size crate
+// Assuming that the crates can be rotated (not individually, all or nothing)
+func fit2(crateX int, crateY int, boxesX int, boxesY int) float64 {
+	return math.Max(fit1(crateX, crateY, boxesX, boxesY), fit1(crateX, crateY, boxesY, boxesX))
 }
